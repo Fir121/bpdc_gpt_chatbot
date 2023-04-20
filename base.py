320dc1c10eaed5e7a51388a04ae4398249175e6d
Fir121 <mohamedfiras2003@gmail.com>
2023-04-20 04:15:44 +0400

    bot base made

--- base.py
+++ base.py
@@ -3,8 +3,37 @@ import os
 os.environ["OPENAI_API_KEY"] = constants.openapi_key
 
 
-from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader
+from llama_index import GPTSimpleVectorIndex
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+from llama_index.langchain_helpers.agents import LlamaToolkit, create_llama_chat_agent, IndexToolConfig
+from langchain.chat_models import ChatOpenAI
 
-documents = SimpleDirectoryReader('data').load_data()
-index = GPTSimpleVectorIndex.from_documents(documents)
-index.save_to_disk('index.json')
+index = GPTSimpleVectorIndex.load_from_disk('index.json')
+indexes = [
+    IndexToolConfig(
+            index=index, 
+            name="Vector Index",
+            description="useful for when you want to answer queries about ACM",
+            index_query_kwargs={"similarity_top_k": 3},
+            tool_kwargs={"return_direct": True}
+    )
+]
+toolkit = LlamaToolkit(
+    index_configs=indexes
+)
+
+memory = ConversationBufferMemory(memory_key="chat_history")
+llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+agent_chain = create_llama_chat_agent(
+    toolkit,
+    llm,
+    memory=memory,
+    verbose=True
+)
+
+while True:
+    text_input = input("User: ")
+    # handle valueerror
+    response = agent_chain.run(input=text_input)
+    print(f'Agent: {response}')
