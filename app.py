6a9c872429888a318a09ba30c0760dbadb9164af
Fir121 <mohamedfiras2003@gmail.com>
2023-04-27 18:00:32 +0400

    flask basic config

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 import flask
-from flask import render_template, session, request
-from flask_session import Session
+from flask import render_template, session, request, abort
+import backend
 
 app = flask.Flask(__name__)
-app.config["SESSION_PERMANENT"] = True
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
 
-@app.route('/', methods=['GET'])
-def home():
-    code = request.args.get("code")
-    #if code is None:
-    return render_template("index.html")
+@app.route('/', methods=['GET','POST'])
+def main_page():
+    if request.method == "POST":
+        fname = request.form.get("fname")
+        if fname is None:
+            chain,fname = backend.create_chain()
+        else:
+            chain = backend.load_chain(fname)
+        message = request.form.get("message")
+        if message is None:
+            return abort(400)
+        message_response = chain.run(message)
+        backend.save_chain(chain, fname)
+        return render_template("index.html", fname=fname, message_response=message_response)
+
+    return render_template("index.html", fname=None, message_response=None)
 
 if __name__ == "__main__":
     app.run()
