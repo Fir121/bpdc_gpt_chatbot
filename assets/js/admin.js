a82d484a292e51e65d32a32ed3b3cf528278b7c6
Fir121 <mohamedfiras2003@gmail.com>
2023-05-06 00:50:26 +0400

    fixed admin page formatting

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,3 +1,13 @@
 const format = (text) => {
     return text.replace(/(?:\r\n|\r|\n)/g, '<br>')
 }
+
+const markdown          = window.markdownit();
+
+document.addEventListener("load", () => {
+    var elems = document.getElementsByClassName("content");
+    for (var i=0; i<elems.length; i++){
+        var text =  elems.innerHTML;
+        elems.innerHTML = markdown.render(text);
+    } 
+});
