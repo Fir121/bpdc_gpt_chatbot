6153f685874f3462796ec15698b15253f9c768a0
Fir121 <mohamedfiras2003@gmail.com>
2023-05-07 19:13:15 +0400

    frontend changes

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -15,3 +15,17 @@ document.addEventListener("load", () => {
         elems.innerHTML = markdown.render(text);
     } 
 });
+
+document.querySelector('.mobile-sidebar').addEventListener('click', (event) => {
+    const sidebar = document.querySelector('.conversations');
+    
+    if (sidebar.classList.contains('shown'))  {
+        sidebar.classList.remove('shown');
+        event.target.classList.remove('rotated');
+    } else {
+        sidebar.classList.add('shown');
+        event.target.classList.add('rotated');
+    }
+
+    window.scrollTo(0, 0);
+})
